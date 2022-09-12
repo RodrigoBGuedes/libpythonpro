83eae666419a318a1213c977bc7fd32fee996153
rodrigo.guedes <gedsao77@hotmail.com>
2022-09-12 16:41:53 -0300

    Feito os testes de Emails

---
+++ libpythonpro/tests/test_spam/teste_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.teste_enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['gedsao77@hotmail.com','rodrigo-guedes@hotmail.com']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+         remetente,
+         'rodrigobuenoguedes@gmail.com',
+         'Cursos Python Pro',
+          'Teste Doidera é isso'
+    )
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'geraldo']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+             remetente,
+             'rodrigobuenoguedes@gmail.com',
+             'Cursos Python Pro',
+              'Teste Doidera é isso'
+        )
+
